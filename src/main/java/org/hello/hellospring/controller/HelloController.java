32f3d38d3a6936ac2fa4709d74deeeb23fc718cf
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-03-26 00:01:17 +0900

    :sparkles: feat: Section 2-3. API 실습

--- src/main/java/org/hello/hellospring/controller/HelloController.java
+++ src/main/java/org/hello/hellospring/controller/HelloController.java
@@ -4,7 +4,9 @@ import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,6 +21,33 @@ public class HelloController {
   public String helloMvc(@RequestParam(value = "name") String name, Model model) {
     model.addAttribute("name", name);
     return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloSpring(@RequestParam(value = "name") String name) {
+    return "hello " + name; // "hello Spring"
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam(value = "name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
+
+  static class Hello {
+
+    private String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 
 }
