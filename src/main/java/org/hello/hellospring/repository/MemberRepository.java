6e3f59c773eef4a097c3eb20a7b5ef7126de99d0
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-04-01 23:16:25 +0900

    :sparkles: feat: Section 3-1. 회원 도메인과 리포지토리 생성 실습

---
+++ src/main/java/org/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package org.hello.hellospring.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.hello.hellospring.domain.Member;
+
+public interface MemberRepository {
+
+  Member save(Member member);
+  Optional<Member> findByID(Long id);
+  Optional<Member> findByName(String name);
+  List<Member> findByAll();
+}
