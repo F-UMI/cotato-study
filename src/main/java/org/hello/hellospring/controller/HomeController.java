4b70579b8dd59b8de808f1b1035fa6a7e13b2cd3
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-04-02 20:54:46 +0900

    :sparkles: feat: Section 5-2.회원 등록 실습

--- src/main/java/org/hello/hellospring/controller/HomeController.java
+++ src/main/java/org/hello/hellospring/controller/HomeController.java
@@ -1,13 +1,42 @@
 package org.hello.hellospring.controller;
 
+import org.hello.hellospring.domain.Member;
+import org.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+  private final MemberService memberService;
+
+  @Autowired
+  public HomeController(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
   @GetMapping("/")
   public String home() {
     return "home";
+  }
+
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+
+//    System.out.println("member.getName() = " + member.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
   }
 }
